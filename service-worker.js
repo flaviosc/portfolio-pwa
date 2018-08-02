dd6b589b9241b487df586c6025aed71d3cd71a75
Flávio Santana <flaviosantana.c@gmail.com>
2018-08-02 02:50:44 -0300

    correção problema service worker

--- service-worker.js
+++ service-worker.js
@@ -1,67 +1,40 @@
-var APP_PREFIX = 'FSDev_';    // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_01';            // Version of the off-line cache (change this value everytime you want to update cache)
-var CACHE_NAME = APP_PREFIX + VERSION;
-var URLS = [                            // Add URL you want to cache in this list.
-  './css/style.css',                     // If you have separate JS/CSS files,
-  'index.html',
-  './js/app.js',
-  '/service-worker.js'            // add path to those files here
-]
-
-var filesToCache = [
-    '/index.html',
-    '/js/app.js',
-    '/images',
-    '/css/style.css'
+let cacheName = 'FSDev-1.0';
+let filesToCache = [
+    './',
+    'index.html',
+    'css/style.css',
+    'js/app.js',
 ];
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
+self.addEventListener('install', function(e){
+    console.log('[ServiceWorker] Installer');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
 
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  )
-})
+self.addEventListener('activate', function(e){
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList){
+            return Promise.all(keyList.map(function(key){
+                if(key !== cacheName){
+                    console.log('[ServiceWorker] Removing old cache');
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
 
-// Cache resources
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(URLS)
-    })
-  )
-})
-
-// Delete outdated caches
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create white list
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      // add current cache name to white list
-      cacheWhitelist.push(CACHE_NAME)
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
-        }
-      }))
-    })
-  )
-})
+self.addEventListener('fetch', function(e){
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response){
+            return response || fetch(e.request);
+        })
+    );
+});
